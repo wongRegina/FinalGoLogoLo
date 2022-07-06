35c662898d5a33830c93ca37e620c2c52a112058
wongRegina <43732940+wongRegina@users.noreply.github.com>
2022-07-06 14:38:12 -0400

    Delete server directory

--- server/config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'googleAuth' : {
-        'clientID'      : '761812037199-7jlur59f74v4mjog800rk0s2lbmgo3dt.apps.googleusercontent.com',
-        'clientSecret'  : 'WgDG0_5ONNpTBoGr_cd7zp49',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
-    }
-};
--- server/routes/google.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-
-module.exports = function (passport) {
-    return router.get('/', passport.authenticate('google', { scope: ['profile', 'email'], prompt : "select_account" }));
-}
